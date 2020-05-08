55efcae5682e09a5a031ba6d83cac5339cbd0286
denis <denis.scarlat@gmail.com>
2020-05-09 00:26:58 +0300

    twiter react app uploaded

---
+++ twitter_react_App/src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css'
+
+const AppHeader = ({liked, allPosts}) => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Denis Scarlat</h1>
+            <h2>{allPosts} posts, liked: {liked}</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
