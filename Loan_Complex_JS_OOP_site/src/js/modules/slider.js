1d6fd4d70d322d8bc8974220bce42601fcd124ae
denis <denis.scarlat@gmail.com>
2020-05-22 20:48:30 +0300

    added hanson slide

--- Loan_Complex_JS_OOP_site/src/js/modules/slider.js
+++ Loan_Complex_JS_OOP_site/src/js/modules/slider.js
@@ -6,17 +6,37 @@ export default class Slider {
         this.btns = document.querySelectorAll(btns);
         this.slideIndex = 1;
     }
-    
-    showSlides(n) {
 
+    showSlides(n) {
         if (n > this.slides.length) {
             this.slideIndex = 1;
         }
         if (n < 1) {
             this.slideIndex = this.slides.length;
         }
-        this.slides.forEach(slide => {
+            //hanson slide
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+
+                this.hanson.classList.add('animated');
+
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (error) {}
+            // hanson slide
+
+            this.slides.forEach(slide => {
+
             slide.style.display = 'none';
+
         });
         this.slides[this.slideIndex - 1].style.display = 'block';
     }
@@ -26,16 +46,22 @@ export default class Slider {
     }
 
     render() {
+        // hanson slide
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (error) {}
+        // hanson slide 
+
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1)
             });
 
             item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                e.preventDefault();                  
+                e.preventDefault();
                 this.slideIndex = 1;
                 this.showSlides(this.slideIndex);
-            })  
+            })
         });
         this.showSlides(this.slideIndex);
     }
