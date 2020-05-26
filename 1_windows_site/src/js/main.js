6a3a09c12e4a4d7601285c90711ceb15a20ce91f
denis <denis.scarlat@gmail.com>
2020-05-26 22:42:39 +0300

    in steklopaketina added tabs.js

--- 1_windows_site/src/js/main.js
+++ 1_windows_site/src/js/main.js
@@ -1,7 +1,9 @@
 
 import './slider';
 import Modals from './modules/modals';
+import Tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     Modals();
+    Tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active')
 });
