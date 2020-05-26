6c1352e6e1ae6c1efbeaaa9f9c261a40e329db50
denis <denis.scarlat@gmail.com>
2020-05-26 22:39:53 +0300

    in steklopaketina added tabs.js

--- 1_windows_site/src/js/modules/modals.js
+++ 1_windows_site/src/js/modules/modals.js
@@ -44,7 +44,7 @@ const Modals = () => {
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
 
-    showModalByTime('.popup', 3000);
+    // showModalByTime('.popup', 60000);
 
 };
 
