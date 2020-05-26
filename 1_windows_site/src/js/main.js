9e31fc15130afdd091333c966a916275ff32a1b0
denis <denis.scarlat@gmail.com>
2020-05-26 23:03:06 +0300

    added tabs

--- 1_windows_site/src/js/main.js
+++ 1_windows_site/src/js/main.js
@@ -4,9 +4,9 @@ import Modals from './modules/modals';
 import Tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
-
+    
     Modals();
     Tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     Tabs('.decoration_slider', '.no_click', '.decoration_content >div >div', 'after_click');
-    
+
 });
