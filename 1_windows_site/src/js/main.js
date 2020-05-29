8580bcaf256bdecc7c354c0fbd693eef57e2d293
denis <denis.scarlat@gmail.com>
2020-05-29 18:39:59 +0300

    added timer

--- 1_windows_site/src/js/main.js
+++ 1_windows_site/src/js/main.js
@@ -4,11 +4,14 @@ import Modals from './modules/modals';
 import Tabs from './modules/tabs';
 import Forms from './modules/forms';
 import ChangeModalState from './modules/changeModalState';
+import Timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict"
 
     let modalState = {};
+
+    let deadLine = '2020-06-15';
 
     ChangeModalState(modalState);
 
@@ -16,7 +19,8 @@ window.addEventListener('DOMContentLoaded', () => {
     Tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     Tabs('.decoration_slider', '.no_click', '.decoration_content >div >div', 'after_click');
     Tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');  //calculator tab  //.big_img > img -- direct children
-   
+
+    Timer('.container1', deadLine);
     Forms(modalState);
 
 });
