868c6651680f1f659c171b24b45e6c309c506570
denis <denis.scarlat@gmail.com>
2020-06-19 11:05:38 +0300

    added forms

--- 3_Loan_Complex_JS_OOP_site/src/js/modules/form.js
+++ 3_Loan_Complex_JS_OOP_site/src/js/modules/form.js
@@ -1,35 +1,104 @@
-
 export default class Form {
-
     constructor(forms) {
         this.forms = document.querySelectorAll(forms);
+        this.inputs = document.querySelectorAll('input');
         this.message = {
             loading: 'loading...',
-            success: 'Thank you, we recieved your data!',
+            success: 'We recievied yor data!',
             failure: 'Error'
         };
         this.path = 'assets/question.php';
     }
 
-    async postData(url, data) {
+    clearInputs() {
+        this.inputs.forEach(item => {
+            item.value = '';
+        });
+    }
 
+    checkMailInputs(){
+        const mailInputs = document.querySelectorAll('[type="email"]');
+    
+        mailInputs.forEach(input => {
+            input.addEventListener('keypress', function(e) {
+                if (e.key.match(/[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                }
+            });
+        });
+    }
+
+    initMask() {
+
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+            
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+
+        function createMask(event) {
+            let matrix = '+1 (___) ___-____',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, '');
+
+            if (def.length >= val.length) {
+                val = def;
+            }
+
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            });
+
+            if (event.type === 'blur') {
+                if (this.value.length == 2) {
+                    this.value = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+        }
+
+        let inputs = document.querySelectorAll('[name="phone"]');
+
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask);
+            input.addEventListener('blur', createMask);
+        });
+    }
+
+    async postData(url, data) {
         let res = await fetch(url, {
-            method: 'POST',
+            method: "POST",
             body: data
-        })
+        });
+    
+        return await res.text();
     }
 
     init() {
-        this.forms.forEach(item => {
+        this.checkMailInputs();
+        this.initMask();
 
+        this.forms.forEach(item => {
             item.addEventListener('submit', (e) => {
                 e.preventDefault();
 
                 let statusMessage = document.createElement('div');
                 statusMessage.style.cssText = `
-                            margin-top: 15px;
-                            font-size: 18px;
-                            color: grey;
+                    margin-top: 15px;
+                    font-size: 18px;
+                    color: grey;
                 `;
                 item.parentNode.appendChild(statusMessage);
 
@@ -39,12 +108,18 @@ export default class Form {
 
                 this.postData(this.path, formData)
                     .then(res => {
-                        console.log('res');
+                        console.log(res);
                         statusMessage.textContent = this.message.success;
                     })
-                    .catch(()=>{
-                        statusMessage.textContent = this.message.failure; 
+                    .catch(() => {
+                        statusMessage.textContent = this.message.failure;
                     })
+                    .finally(() => {
+                        this.clearInputs();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 6000);
+                    });
             });
         });
     }
