72db150135a88a4a44dd355744c91ac9f974d1b4
denis <denis.scarlat@gmail.com>
2020-07-29 13:49:01 +0300

    added blog author page with routing buttons

--- angular-blog/src/app/app-routing.module.ts
+++ angular-blog/src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: PreloadAllModules 
   })],
   exports: [RouterModule],
 })
