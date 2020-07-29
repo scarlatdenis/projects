cbd274a2962cdef59da047aa131a6aacf46cf8bc
denis <denis.scarlat@gmail.com>
2020-07-29 14:54:58 +0300

    added admin pages layout

--- angular-blog/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ angular-blog/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  logout(event: Event) {
+    event.preventDefault()
 
-  ngOnInit(): void {
+    this.router.navigate(['/admin','login'])
+    
+
   }
-
 }
