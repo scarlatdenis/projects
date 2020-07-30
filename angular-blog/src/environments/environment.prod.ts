bb6bd710a1f526d2eaafcba5013e7cda5e89df78
denis <denis.scarlat@gmail.com>
2020-07-30 16:14:47 +0300

    am setat firebase si token

--- angular-blog/src/environments/environment.prod.ts
+++ angular-blog/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDg2qat1KEqbhT9xUCiUFgt4OKiIWVtnFU'
 };
